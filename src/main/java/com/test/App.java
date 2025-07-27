c72e584ee54415a09d61cc0a8936efcf2e603d31
Anupam <anupam12597@gmail.com>
2025-07-27 15:10:34 +0530

    m6

--- src/main/java/com/test/App.java
+++ src/main/java/com/test/App.java
@@ -5,6 +5,10 @@ package com.test;
  */
 public class App {
     public static void main(String[] args) {
+<<<<<<< HEAD
         System.out.println("Hello hello World!............. how are you");
+=======
+        System.out.println("Hello hello World!.............india");
+>>>>>>> 55a78713e82d78014af76b20f1b7a7ddff3ab368
     }
 }
